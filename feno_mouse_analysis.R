7493cfb8f9b0b38e546076c5f8d30e7984e7ad13
austinchia <austin.chia.ce@gmail.com>
2022-07-07 11:14:15 +0800

    added pca method 2

--- feno_mouse_analysis.R
+++ feno_mouse_analysis.R
@@ -15,7 +15,7 @@ library(dplyr)
 library(data.table)
 library(stringr)
 library(tidyverse)
-
+library(ggrepel)
 # ============== 1. Read & Selects from Excel File ======
 
 # reads raw data
@@ -23,11 +23,11 @@ feno_raw <- read_excel('Feno_Mouse_Dataset.xlsx', sheet = 'Feno_Mouse_Dataset', 
   # replaces "Filtered" with NAs
   na_if(., "Filtered") %>%
   
-  # removes NAs
-  na.omit() %>%
-  
   # renames Gene Symbol column
   rename("Gene Symbol" = "PG.ProteinNames") %>%
+  
+  # removes NAs in Gene Symbol
+  na.omit(`Gene Symbol`) %>%
   
   # adds number to the end of duplicate gene symbols (ie Sptbn1-2)
   group_by(`Gene Symbol`) %>%
@@ -41,11 +41,22 @@ feno_raw <- read_excel('Feno_Mouse_Dataset.xlsx', sheet = 'Feno_Mouse_Dataset', 
   select(., -c(`PG.ProteinAccessions`, `PG.ProteinDescriptions`)) %>%
   
   mutate_all(function(x) as.numeric(as.character(x)))
+library(IMIFA)
+transform_data <- function(x) {
+  x <- log2(x)
+  rowmed <- apply(x,1,median)
+  x <- sweep(x,1,rowmed,"-")
+  x <- data.frame(pareto_scale(x, centering = TRUE))
+} 
+# applies function to transform data
+feno_normalized <- transform_data(feno_raw)
 
-# plots pca
+#  === runs method 1 pca =====
 
-pca_res <- prcomp(feno_raw, scale=TRUE)
+pca_res <- prcomp(t(feno_normalized), scale=TRUE)
 summary(pca_res)
+
+
 
 var_explained <- pca_res$sdev^2/sum(pca_res$sdev^2)
 
@@ -56,3 +67,20 @@ pca_res$x %>%
   labs(x=paste0("PC1: ",round(var_explained[1]*100,1),"%"),
        y=paste0("PC2: ",round(var_explained[2]*100,1),"%")) +
   theme(legend.position="top")
+
+# ==== runs method 2 pca =====
+
+pca.var <- pca_res$sdev^2
+pca.var.per <- round(pca.var/sum(pca.var)*100, 1)
+
+pca.data <- data.frame(Sample = rownames(pca_res$x),
+                       X = pca_res$x[,1],
+                       Y = pca_res$x[,2])
+
+ggplot(data=pca.data, aes(x=X, y=Y, label=Sample)) +
+  geom_text_repel(max.overlaps = Inf, point.size = NA) +
+  geom_text() +
+  xlab(paste("PC1 - ", pca.var.per[1], "%", sep="")) +
+  ylab(paste("PC2 - ", pca.var.per[2], "%", sep="")) +
+  theme_bw() +
+  ggtitle("My PCA Graph")
